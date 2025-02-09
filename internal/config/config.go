3aad17b3b79216ad291c226a37f497b58302645d
Mykhailo Fiialka <mfiyalka@gmail.com>
2025-02-09 16:19:28 +0200

    Added empty folders

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,7 +2,8 @@ package config
 
 import "github.com/joho/godotenv"
 
-func Load(path string) error { //nolint:revive
+// Load loads environment variables from a file specified by the given path.
+func Load(path string) error {
 	err := godotenv.Load(path)
 	if err != nil {
 		return err
